69a09b5d740680e22021cccadd7dd3e1312051bf
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-22 16:11:20 +0530

    win check

--- main.py
+++ main.py
@@ -22,13 +22,15 @@ class GridBox:
 
     def update(self, current):
         self.surface.fill((255, 255, 255))
+        if self.typ is not None:
+            self.surface.blit(self.assets[self.typ], (self.center_rect.x, self.center_rect.y))
+        if current is None:
+            return
         if self.typ is None:
             if self.rect.collidepoint(pygame.mouse.get_pos()):
                 if pygame.mouse.get_pressed()[0]:
                     self.typ = current
                     return True
-        if self.typ is not None:
-            self.surface.blit(self.assets[self.typ], (self.center_rect.x, self.center_rect.y))
 
 class Game:
     def __init__(self):
@@ -40,8 +42,21 @@ class Game:
         self.game_grid = [[0]*3, [0]*3, [0]*3]
         for i in range(9):
             self.game_grid[i//3][(i%3)] = GridBox(i)
-        print(self.game_grid)
+
         self.current = 0
+        self.win = False
+
+    def check_win(self):
+        combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        flat_grid = []
+        for y in self.game_grid:
+            for x in y:
+                flat_grid.append(x)
+        for c in combinations:
+            if flat_grid[c[0]].typ == flat_grid[c[1]].typ == flat_grid[c[2]].typ != None:
+                return str(flat_grid[c[0]].typ)
+        return False
+
 
     def run(self):
         while self.RUNNING:
@@ -51,9 +66,13 @@ class Game:
             self.screen.fill((255, 255, 255))
             for y in self.game_grid:
                 for x in y:
-                    e = x.update(self.current)
+                    e = x.update(self.current if not self.win else None)
                     if e:
                         self.current = 1 if self.current == 0 else 0
+                        win = self.check_win()
+                        if win:
+                            print(f"{win} Won!")
+                            self.win = True
                     self.screen.blit(x.surface, (x.x, x.y))
             self.CLOCK.tick(60)
             pygame.display.update()
