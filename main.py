c8720ed5dbea6e18398f6be800524e41abf0d612
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-22 17:00:57 +0530

    crossing on win

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ class Game:
 
         self.current = 0
         self.win = False
+        self.win_line = []
 
     def check_win(self):
         combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
@@ -54,6 +55,8 @@ class Game:
                 flat_grid.append(x)
         for c in combinations:
             if flat_grid[c[0]].typ == flat_grid[c[1]].typ == flat_grid[c[2]].typ != None:
+                self.win_line.append((flat_grid[c[0]].x+75, flat_grid[c[0]].y+75))
+                self.win_line.append((flat_grid[c[2]].x+75, flat_grid[c[2]].y+75))
                 return str(flat_grid[c[0]].typ)
         return False
 
@@ -71,9 +74,12 @@ class Game:
                         self.current = 1 if self.current == 0 else 0
                         win = self.check_win()
                         if win:
-                            print(f"{win} Won!")
+                            p = ["O", "X"]
+                            print(f"{p[int(win)]} Won!")
                             self.win = True
                     self.screen.blit(x.surface, (x.x, x.y))
+            if self.win:
+                pygame.draw.line(self.screen, (0, 0, 0), self.win_line[0], self.win_line[1], 8)
             self.CLOCK.tick(60)
             pygame.display.update()
 
