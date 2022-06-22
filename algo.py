a9c7e0f0e6fa7b43e53092129149548f02f934a9
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-06-22 23:44:51 +0530

    somewaht minmax but with 1

--- algo.py
+++ algo.py
@@ -3,9 +3,19 @@ import random
 def playmove(grid):
     index = 0
     available = []
+    best = -10000000000000
+    bestmove = None
     for y in grid:
         for x in y:
             if x.typ == None:
-                available.append(index)
+                grid[index//3][index%3].typ = 1
+                score = minimax(grid)
+                grid[index//3][index%3].typ = None
+                if score > best:
+                    best = score
+                    bestmove = index
             index+=1
-    return random.choice(available) if len(available) > 0 else None
+    return bestmove
+
+def minimax(grid):
+    return 1
